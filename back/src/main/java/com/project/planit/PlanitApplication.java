54ab996e10fcba6ae68c088b1f62017381af48ef
Gukss <dongkuk97@naver.com>
2023-01-18 16:00:34 +0900

    backend project init

---
+++ back/src/main/java/com/project/planit/PlanitApplication.java
@@ -0,0 +1,13 @@
+package com.project.planit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanitApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/project/planit/PlanitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.planit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
