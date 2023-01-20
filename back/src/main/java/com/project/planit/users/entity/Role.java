846e1daee6d1845c67aeac148fe4fe2893e14691
Gukss <dongkuk97@naver.com>
2023-01-20 15:31:22 +0900

    FIX: user 엔티티 추가

---
+++ back/src/main/java/com/project/planit/users/entity/Role.java
@@ -0,0 +1,12 @@
+package com.project.planit.users.entity;
+
+/**
+ * packageName    : com.project.planit.users.entity fileName       : Role author         : SSAFY
+ * date           : 2023-01-20 description    :
+ * =========================================================== DATE              AUTHOR
+ * NOTE ----------------------------------------------------------- 2023-01-20        SSAFY       최초
+ * 생성
+ */
+public enum Role {
+  ADMIN, MEMBER
+}
---
+++ back/src/main/java/com/project/planit/util/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.project.planit.util;
+
+/**
+ * packageName    : com.project.planit.util fileName       : BaseEntity author         : SSAFY date
+ *          : 2023-01-20 description    :
+ * =========================================================== DATE              AUTHOR
+ * NOTE ----------------------------------------------------------- 2023-01-20        SSAFY       최초
+ * 생성
+ */
+public class BaseEntity {
+  private String constructor;
+  private String updator;
+  private String created_at;
+  private String updated_date;
+}
