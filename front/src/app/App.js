852acd5a0306a0f09f2e917f833d5a9666071544
dongminYOUN <kkang15794@naver.com>
2023-01-27 12:31:06 +0900

    DOCS: FE_로그인Markup완성, FEAT: FE_회원가입Markup

--- front/src/app/App.js
+++ front/src/app/App.js
@@ -2,12 +2,14 @@ import './App.module.scss';
 import { Routes, Route } from 'react-router-dom';
 import Room from '../routes/room/Room';
 import Login from '../routes/auth/login/Login';
+import SignUp from '../routes/auth/signup/SignUp';
 
 function App() {
   return (
     <Routes>
       <Route path='/room/*' element={<Room />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
     </Routes>
   );
 }
---
+++ front/src/routes/home/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home1 from './home1/Home1';
+
+function HomeLayout() {
+  return (
+    <div>
+      <Home1 />
+    </div>
+  );
+}
+
+export default HomeLayout;
