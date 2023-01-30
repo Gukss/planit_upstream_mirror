fe56fd72f1711fb3a86526b9a3cdafd74c59903e
Gukss <dongkuk97@naver.com>
2023-01-30 14:22:32 +0900

    FEAT: RoomController.readRoom 완료
    RoomController.readRoom 완료-
    ReadRoomResponse 생성-

--- back/src/main/java/com/project/planit/room/controller/RoomController.java
+++ back/src/main/java/com/project/planit/room/controller/RoomController.java
@@ -9,8 +9,11 @@ import com.project.planit.room.service.RoomService;
 import com.project.planit.vote.dto.CreateVoteResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,5 +46,13 @@ public class RoomController {
     UpdateRoomResponse updateRoomResponse = UpdateRoomResponse.create(updatedRoom.getId(),
         updatedRoom.getRoomName(), request.getStartDate(), request.getEndDate());
     return ResponseEntity.ok(updateRoomResponse);
+  }
+
+  @GetMapping(path = "{roomId}")
+  public ResponseEntity<?> readRoom(@PathVariable Long roomId){
+    System.out.println(roomId);
+    Room room = roomService.findById(roomId);
+    //todo: 프론트랑 반환값 상의해서 넣어주기
+    return ResponseEntity.ok(HttpStatus.OK);
   }
 }
