58c1c80466a5411f4552e00762ce7c886fcb3032
김동국 <dongkuk97@naver.com>
2023-01-30 21:08:46 +0900

    Revert "Merge branch 'BE_feature' into 'master'"
    This reverts merge request !20

--- back/src/main/java/com/project/planit/PlanitApplication.java
+++ back/src/main/java/com/project/planit/PlanitApplication.java
@@ -2,10 +2,8 @@ package com.project.planit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class PlanitApplication {
 
 	public static void main(String[] args) {
