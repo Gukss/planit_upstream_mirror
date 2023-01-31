4e1ea3d80ef36007bc279d1e032980d79ba3fa93
eondo <vov06144@naver.com>
2023-01-31 17:56:51 +0900

    FEAT: FE_검색 기능 추가

--- front/src/routes/room/bbarcontent/search/ResultList.jsx
+++ front/src/routes/room/bbarcontent/search/ResultList.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ResultListItem from './ResultListItem';
 import classes from './ResultList.module.scss';
 
-function ResultList() {
+function ResultList(props) {
   return (
     <div className={classes.result}>
-      <ResultListItem />
+      {/* {props.resultPlaces.map(place => (
+        <div key={place.address_name}>
+          <p>{place.place_name}</p>
+          <p>{place.x}</p>
+          <p>{place.y}</p>
+          <hr />
+        </div>
+      ))} */}
+      {props.resultPlaces.map(place => {
+        return <ResultListItem place={place} />;
+      })}
     </div>
   );
 }
--- front/src/routes/room/bbarcontent/search/ResultListItem.jsx
+++ front/src/routes/room/bbarcontent/search/ResultListItem.jsx
@@ -4,14 +4,15 @@ import classes from './ResultListItem.module.scss';
 
 const items = [1, 2, 3];
 
-function ResultListItem() {
+function ResultListItem(props) {
   return (
     <div className={classes.resultitem}>
-      <div className={classes.resultitem__title}>title</div>
+      <div className={classes.resultitem__title}>{props.place.place_name}</div>
       <div className={classes.resultitem__content}>
         <div>별점4.5</div>
         <div>리뷰 500</div>
         <div>ㅇㅇ시 ㅇㅇ구 ㅇㅇ동 1000</div>
+        <div>{props.place.place_url}</div>
       </div>
       <div className={classes.resultitem__photo}>
         {items.map(item => (
