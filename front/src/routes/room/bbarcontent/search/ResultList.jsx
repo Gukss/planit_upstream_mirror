f971cf375cef542c28c3489facae777698d030bc
김도언 <vov06144@naver.com>
2023-02-02 15:14:00 +0900

    FEAT: FE_Search 목록창 스크롤 및 검색창 첫 화면 구현

--- front/src/routes/room/bbarcontent/search/ResultList.jsx
+++ front/src/routes/room/bbarcontent/search/ResultList.jsx
@@ -3,15 +3,24 @@ import React from 'react';
 import ResultListItem from './ResultListItem';
 import classes from './ResultList.module.scss';
 import { searchedPlaces } from '../../../../app/store';
+import logo1 from '../../../../app/assets/images/logo1.png';
 
 function ResultList() {
   const resultPlaces = useRecoilValue(searchedPlaces);
 
+  if (resultPlaces.length >= 1) {
+    return (
+      <div className={classes.result}>
+        {resultPlaces.map(place => {
+          return <ResultListItem place={place} />;
+        })}
+      </div>
+    );
+  }
   return (
-    <div className={classes.result}>
-      {resultPlaces.map(place => {
-        return <ResultListItem place={place} />;
-      })}
+    <div className={classes.result_first}>
+      <img src={logo1} alt='logo1' />
+      <p>지금 당장 떠나고 싶은 곳은?</p>
     </div>
   );
 }
