a25216f8f65fc7a3782a7e5b3d47bfa2bac98103
dongminYOUN <kkang15794@naver.com>
2023-02-02 13:36:06 +0900

    FEAT: FE_kakao 검색후 지도이동 및 마커표시

--- front/src/routes/room/map/Map.jsx
+++ front/src/routes/room/map/Map.jsx
@@ -1,102 +1,90 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useRecoilValue } from 'recoil';
+import { searchedPlaces, searchedPlace } from '../../../app/store';
 import './Map.scss';
 
 const { kakao } = window;
-const title = '카카오 스페이스';
-const markerPositions = [
-  {
-    content: `
-      <div class='wrap'> <div class='info'>
-              <div class='title'>
-                  ${title}
-                  <button class="close" onClick={} >x</ button>
-              </div>
-              <div class="body">
-                  <div class="img">
-                      <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">
-                 </div>
-                  <div class="desc">
-                      <div class="ellipsis">제주특별시 제주시 첨단로 242</div>
-                      <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>
-                      <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>
-                  </div>
-              </div>
-          </div>
-      </div>`,
+let markers = []; // 마커정보 저장
+// const title = '제목';
+// const markerPositions = [
+//   {
+//     content: `<div class='title'>${title}</div>`,
+//     latlng: new kakao.maps.LatLng(33.450936, 126.569477),
+//     // latlng: new kakao.maps.LatLng(searchData[0].x, searchData[0].y),
+//   },
+// ];
 
-    latlng: new kakao.maps.LatLng(33.450705, 126.570677),
-  },
-  {
-    content: "<div class='title'>생태연못</div>",
-    latlng: new kakao.maps.LatLng(33.450936, 126.569477),
-  },
-  {
-    content: '<div>텃밭</div>',
-    latlng: new kakao.maps.LatLng(33.450879, 126.56994),
-  },
-  {
-    content: '<div>근린공원</div>',
-    latlng: new kakao.maps.LatLng(33.451393, 126.570738),
-  },
-];
-
-function makeOverListener(map, marker, infowindow) {
-  return function () {
-    infowindow.open(map, marker);
-  };
-}
-
-// 인포윈도우를 닫는 클로저를 만드는 함수입니다
-function makeOutListener(infowindow) {
-  return function () {
-    infowindow.close();
-  };
-}
-
-// 창닫기
-function closeOverlay(infowindow) {
-  infowindow.close();
-}
+// 마커 생성 및 지도 표시
+const displayMarker = (place, map) => {
+  const marker = new kakao.maps.Marker({
+    map,
+    position: new kakao.maps.LatLng(place.y, place.x),
+  });
+  marker.setMap(map);
+  markers.push(marker);
+};
+// 마커 지우기
+const removeMarker = () => {
+  for (let i = 0; i < markers.length; i += 1) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+};
 
 function Map() {
   const mapContainer = useRef(null);
-  const position = new kakao.maps.LatLng(33.450701, 126.570667);
-  // const markerPosition = new kakao.maps.LatLng(33.440601, 126.570666);
+  const searchData = useRecoilValue(searchedPlaces);
+  const clickData = useRecoilValue(searchedPlace);
 
+  // 초기 맵 셋팅
+  let position = new kakao.maps.LatLng(33.450701, 126.570667);
   const mapOptions = {
     center: position, // 지도의 중심좌표
     level: 4, // 지도의 확대 레벨
   };
 
-  useEffect(() => {
-    const map = new kakao.maps.Map(mapContainer.current, mapOptions);
-    // const marker = new kakao.maps.Marker({ position: markerPosition }); // 마커 생성
-    // marker.setMap(map);
-    for (let i = 0; i < markerPositions.length; i += 1) {
-      const marker = new kakao.maps.Marker({
-        map, // 마커를 표시할 지도
-        position: markerPositions[i].latlng, // 마커의 위치
-      });
-      marker.setMap(map);
-      const infowindow = new kakao.maps.InfoWindow({
-        content: markerPositions[i].content, // 인포윈도우에 표시할 내용
-      });
+  const [map, setNewMap] = useState(null);
 
-      // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-      // 이벤트 리스너로는 클로저를 만들어 등록합니다
-      // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-      kakao.maps.event.addListener(
-        marker,
-        'click',
-        makeOverListener(map, marker, infowindow)
-      );
-      // kakao.maps.event.addListener(
-      //   marker,
-      //   'mouseout',
-      //   makeOutListener(infowindow)
-      // );
-    }
+  useEffect(() => {
+    const map1 = new kakao.maps.Map(mapContainer.current, mapOptions);
+    setNewMap(map1);
   }, []);
+
+  // 검색 시 바로 여러 마커 표시되게 하는법
+  useEffect(() => {
+    if (searchData.length >= 1) {
+      removeMarker();
+      // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해 LatLngBounds 객체에 좌표를 추가합니다
+      const bounds = new kakao.maps.LatLngBounds();
+
+      for (let i = 0; i < searchData.length; i += 1) {
+        displayMarker(searchData[i], map);
+        bounds.extend(new kakao.maps.LatLng(searchData[i].y, searchData[i].x));
+      }
+      map.setBounds(bounds); // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+    }
+  }, [searchData]);
+
+  // 클릭시 해당 위치로 이동.
+  useEffect(() => {
+    if (Object.keys(clickData).length >= 1) {
+      position = new kakao.maps.LatLng(clickData.x, clickData.y);
+      // LatLngBounds 객체에 좌표를 추가합니다
+      if (Object.keys(clickData).length > 0) {
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setCenter(new kakao.maps.LatLng(clickData.y, clickData.x));
+        map.setLevel(0);
+        // 마커 추가
+        const marker = new kakao.maps.Marker({
+          map,
+          position,
+        });
+        marker.setMap(map);
+      }
+    }
+  }, [clickData]);
+
+  //   map.relayout();
 
   return (
     <div className='map' ref={mapContainer}>
@@ -106,44 +94,3 @@ function Map() {
 }
 
 export default Map;
-
-// import React, { useEffect, useRef } from 'react';
-// import './Map.scss';
-
-// const { kakao } = window;
-// const title = '제목';
-// const markerPositions = [
-//   {
-//     content: `<div class='title'>${title}</div>`,
-//     latlng: new kakao.maps.LatLng(33.450936, 126.569477),
-//   },
-// ];
-
-// function Map() {
-//   const mapContainer = useRef(null);
-//   const position = new kakao.maps.LatLng(33.450701, 126.570667);
-//   const mapOptions = {
-//     center: position, // 지도의 중심좌표
-//     level: 4, // 지도의 확대 레벨
-//   };
-
-//   useEffect(() => {
-//     const map = new kakao.maps.Map(mapContainer.current, mapOptions);
-
-//     for (let i = 0; i < markerPositions.length; i += 1) {
-//       const overlay = new kakao.maps.CustomOverlay({
-//         content : markerPositions[i].latlng
-//         map: map,
-//         position: marker.getPosition(),
-//       });
-//     }
-//   }, []);
-
-//   return (
-//     <div className='map' ref={mapContainer}>
-//       <div></div>
-//     </div>
-//   );
-// }
-
-// export default Map;
