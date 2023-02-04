55cfc5378efd29d3aafaf4b0c18303f7bb4798e3
Gukss <dongkuk97@naver.com>
2023-02-04 23:13:46 +0900

    FEAT: ChattingMessage 반환값 변경 완료
    ChattingMessage 반환값 변경 완료

--- back/src/main/java/com/project/planit/chatting/controller/ChattingController.java
+++ back/src/main/java/com/project/planit/chatting/controller/ChattingController.java
@@ -1,10 +1,16 @@
 package com.project.planit.chatting.controller;
 
 import com.project.planit.chatting.dto.CreateChattingRequest;
+import com.project.planit.chatting.dto.FindChattingMessageResponse;
+import com.project.planit.chatting.entity.ChattingMessage;
 import com.project.planit.chatting.service.ChattingServiceImpl;
+import com.project.planit.voteItem.dto.FindVoteItemResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,13 +20,24 @@ public class ChattingController {
 
     @PostMapping
     public ResponseEntity<?> createChattingMessage(@RequestBody CreateChattingRequest request){
-        chattingService.createChattingMessage(request,1L);
+        //todo: memberId 토큰값으로 바꿔주기
+        Long memberId = 1L;
+        chattingService.createChattingMessage(request,memberId);
         return ResponseEntity.ok("ok");
     }
 
     @GetMapping(path = "/{roomId}")
-    public ResponseEntity<?> findChattingMessage(@PathVariable Long roomId){
-        return ResponseEntity.ok(chattingService.findChattingMessage(1L,roomId));
+    public ResponseEntity<List<FindChattingMessageResponse>> findChattingMessage(@PathVariable Long roomId){
+        //todo: memberId 토큰 값으로 변경하기
+        Long memberId = 1L;
+        List<ChattingMessage> foundChattingMessage = chattingService.findChattingMessage(memberId, roomId);
+        List<FindChattingMessageResponse> resList = new ArrayList<>();
+
+        for(ChattingMessage chattingMessage: foundChattingMessage){
+            resList.add(chattingMessage.createFindChattingMessageResponse());
+        }
+
+        return ResponseEntity.ok().body(resList);
     }
 
 }
---
+++ back/src/main/java/com/project/planit/chatting/dto/FindChattingMessageResponse.java
@@ -0,0 +1,26 @@
+package com.project.planit.chatting.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * packageName    : com.project.planit.chatting.dto
+ * fileName       : FindChattingMessageResponse
+ * author         : dongk
+ * date           : 2023-02-04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-02-04        dongk       최초 생성
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class FindChattingMessageResponse {
+    private String message;
+    private String constructor;
+    private LocalDateTime createdAt;
+}
