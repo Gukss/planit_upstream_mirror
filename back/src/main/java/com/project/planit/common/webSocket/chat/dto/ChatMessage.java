689dfb9f8a54455d396edcca0ac97bb5a51130f4
김동국 <dongkuk97@naver.com>
2023-02-06 10:29:32 +0900

    Revert "FEAT: security 작업 중"
    This reverts commit ee726c2ca6ae7153d332f9d2c47879c1a667c68c

--- back/src/main/java/com/project/planit/common/webSocket/chat/dto/ChatMessage.java
+++ back/src/main/java/com/project/planit/common/webSocket/chat/dto/ChatMessage.java
@@ -31,7 +31,7 @@ public class ChatMessage {
     }
     @Enumerated(EnumType.STRING)
     private MessageType type; //메시지 타입
-    private String roomId; //방번호
+    private Long roomId; //방번호
     private String sender; //메시지 보낸 사람
     private String message; //메시지
 }
--- back/src/main/java/com/project/planit/common/webSocket/chat/repository/ChatRoomRepository.java
+++ back/src/main/java/com/project/planit/common/webSocket/chat/repository/ChatRoomRepository.java
@@ -35,9 +35,9 @@ public class ChatRoomRepository {
     // Redis
     private static final String CHAT_ROOMS = "CHAT_ROOM";
     private final RedisTemplate<String, Object> redisTemplate;
-    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
+    private HashOperations<String, Long, ChatRoom> opsHashChatRoom;
     // 채팅방의 대화 메시지를 발행하기 위한 redis topic 정보. 서버별로 채팅방에 매치되는 topic정보를 Map에 넣어 roomId로 찾을수 있도록 한다.
-    private Map<String, ChannelTopic> topics;
+    private Map<Long, ChannelTopic> topics;
 
     @PostConstruct
     private void init() {
@@ -49,14 +49,14 @@ public class ChatRoomRepository {
         return opsHashChatRoom.values(CHAT_ROOMS);
     }
 
-    public ChatRoom findRoomById(String id) {
+    public ChatRoom findRoomById(Long id) {
         return opsHashChatRoom.get(CHAT_ROOMS, id);
     }
 
     /**
      * 채팅방 생성 : 서버간 채팅방 공유를 위해 redis hash에 저장한다.
      */
-    public ChatRoom createChatRoom(String id, String name) {
+    public ChatRoom createChatRoom(Long id, String name) {
         ChatRoom chatRoom = ChatRoom.create(id, name);
         opsHashChatRoom.put(CHAT_ROOMS, chatRoom.getRoomId(), chatRoom);
         return chatRoom;
@@ -65,16 +65,16 @@ public class ChatRoomRepository {
     /**
      * 채팅방 입장 : redis에 topic을 만들고 pub/sub 통신을 하기 위해 리스너를 설정한다.
      */
-    public void enterChatRoom(String roomId) {
+    public void enterChatRoom(Long roomId) {
         ChannelTopic topic = topics.get(roomId);
         if (topic == null) {
-            topic = new ChannelTopic(roomId);
+            topic = new ChannelTopic(roomId.toString());
             redisMessageListener.addMessageListener(redisSubscriber, topic);
             topics.put(roomId, topic);
         }
     }
 
-    public ChannelTopic getTopic(String roomId) {
+    public ChannelTopic getTopic(Long roomId) {
         return topics.get(roomId);
     }
 }
