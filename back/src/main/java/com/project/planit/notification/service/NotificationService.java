91a964fb909311f774a6eef057f7675ba3026315
김동국 <dongkuk97@naver.com>
2023-02-11 16:35:10 +0900

    FIX: build 수정

--- back/src/main/java/com/project/planit/notification/service/NotificationService.java
+++ back/src/main/java/com/project/planit/notification/service/NotificationService.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 public interface NotificationService {
     List<FindNotificationResponse> findNotification(String memberAppId, Long id);
-    void createNotification(List<CreateNotificationRequest> request);
-    void updateNotification(UpdateNotificationRequest request);
+    void createNotification(List<CreateNotificationRequest> request, Long id);
+    void updateNotification(UpdateNotificationRequest request, Long id);
     SseEmitter subscribe(Long memberId, String lastEventId);
     void sendToClient(SseEmitter emitter, String id, Object data, String target);
     void send(Long userId, String value, String target);
